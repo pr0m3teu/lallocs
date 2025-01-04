0301f2cffde4438f492e9c3b0efbb55d10fd57d5
Andrei Mihai <andreimihai2305is@gmail.com>
2025-01-04 11:20:56 +0200

    finished lmalloc

--- lallocs.h
+++ lallocs.h
@@ -21,6 +21,14 @@ typedef struct {
 void* lmalloc(List* ptrs, uint64_t size);
 
 #ifdef LALLOCS_IMPLEMENTATION
+static void check_ptr(void* ptr)
+{
+    if (ptr == NULL)
+    {
+        fprintf(stderr, "ERROR: Could not allocate any more memory\n");
+        exit(1);
+    }
+}
 
 void* lmalloc(List* ptrs, uint64_t size)
 {
@@ -29,14 +37,33 @@ void* lmalloc(List* ptrs, uint64_t size)
        fprintf(stderr, "ERROR: List param cannot be NULL\n");
        exit(1);
     }
+
+    if (ptrs->start == NULL)
+    {
+        // This ptrs->start should also be deallocated at some point
+        ptrs->start = malloc(sizeof(Node));
+        check_ptr(ptrs->start);
+
+        ptrs->start->right = NULL;
+        ptrs->start->left = NULL;
+
+        void* ptr = malloc(size);
+        check_ptr(ptr);
+
+        ptrs->start->ptr = ptr;
+        return ptr;
+    }
+
     void* ptr = malloc(size);
-    Node* p = List->start;
+    check_ptr(ptr);
+    Node* p = ptrs->start;
 
     while(p->right != NULL)
     {
         p = p->right;
     }
     p->right = malloc(sizeof(Node));
+    check_ptr(ptr);
 
     p->right->left = p;
     p->right->right = NULL;
--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
+#include <string.h>
+
 #define LALLOCS_IMPLEMENTATION
 #include "lallocs.h"
 
 int main(void)
 {
     List ptrs = {0};
-    char* str = lmalloc(&ptrs, sizeof(char) * 10);
+    char* str = lmalloc(&ptrs, sizeof(char) * 13);
+    strncpy(str, "Hello World\n", 13);
+    printf("%s\n", str);
+    
+    printf("ptrs->start: %p\n", ptrs.start);
+    printf("ptrs->start->right: %p\n", ptrs.start->right);
+    printf("ptrs->start->ptr: %p\n", ptrs.start->ptr);
+    printf("str: %p\n", str);
+
+
    // // do some stuff ig
    // 
    // lfree(str);
@@ -14,6 +25,5 @@ int main(void)
    //     fprintf(stderr, "Memory was not properly deallocated\n");
    //     exit(1);
    // }
-    printf("Hello World\n");
     return 0;
 }
